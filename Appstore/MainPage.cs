b772fc214a5afa69e5877f0ac8faab19b1f90ad6
Christopher Aliprantis <aliprantis@proton.me>
2026-02-19 08:31:42 -0500

    setup in aminpage

--- Appstore/MainPage.cs
+++ Appstore/MainPage.cs
@@ -4,6 +4,11 @@ public sealed partial class MainPage : Page
 {
     public MainPage()
     {
-        
+        var H = new Grid
+        {
+            Width = 1000,
+            Height = 1000,
+        };
+        this.Content = H;
     }
 }
